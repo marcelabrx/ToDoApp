9c9ad00ea2ce6ad4aa560b51e26e7bda48e23751
Marcela Britos <marcelal.britos@gmail.com>
2023-08-11 19:22:09 -0300

    change name ContentTask

--- src/components/contentTask.jsx
+++ src/components/contentTask.jsx
@@ -44,6 +44,7 @@ function ContentTask () {
                         setGetTasks={setGetTasks}
                         handleDoneButton={handleDoneButton}
                         />
+                        
                     )
                 })}
                 
